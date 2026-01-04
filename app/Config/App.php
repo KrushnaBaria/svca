2af4f2e882574fe984e33c80e910675457c13b1b
krishna <krishnabaria07@gmail.com>
2026-01-04 10:12:40 +0530

    update ci

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.43.84/ci';
+    public string $baseURL = 'ttp://10.176.161.22/svca/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
