8b1b10871b225ad72007381f46003b2a7680518e
Krushna <krushnabaria07@gmail.com>
2026-03-20 20:18:53 +0530

    add dummy data to expense and profit chart for ui

--- app/Views/statistics.php
+++ app/Views/statistics.php
@@ -1,27 +1,27 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-md-6 col-12">
         <div class="card">
             <div class="card-header bg-primary d-flex">
                 <h4 class="card-title text-white">Profit Chart</h4>
                 <div class="col-md-2 col-3 ms-auto float-end mb-0">
-                    <input type="text" class="form-control text-white" id="year-filter" onkeydown="return false;">
+                    <input type="text" class="form-control text-white" id="p-year-filter" onkeydown="return false;">
                 </div>
             </div>
             <div class="card-body">
-                <div id=""></div>
+                <div id="profit-chart"></div>
             </div>
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6 col-12">
         <div class="card">
             <div class="card-header bg-primary d-flex">
                 <h4 class="card-title text-white">Expense Chart</h4>
                 <div class="col-md-2 col-3 ms-auto float-end mb-0">
-                    <input type="text" class="form-control text-white" id="year-filter" onkeydown="return false;">
+                    <input type="text" class="form-control text-white" id="e-year-filter" onkeydown="return false;">
                 </div>
             </div>
             <div class="card-body">
-                <div id=""></div>
+                <div id="expense-chart"></div>
             </div>
         </div>
     </div>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -424,6 +424,96 @@
             SVCAobj.recentInquey();
         },
 
+        loadProfitChart: function () {
+
+        },
+
+        initStatistics: function () {
+            let SVCAobj = this;
+            $('#p-year-filter, #e-year-filter').yearpicker();
+
+            var options = {
+                series: [{
+                        name: "Session Duration",
+                        data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+                    },
+                    {
+                        name: "Page Views",
+                        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+                    },
+                    {
+                        name: 'Total Visits',
+                        data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+                    }
+                ],
+                chart: {
+                    height: 350,
+                    type: 'line',
+                    zoom: {
+                        enabled: false
+                    },
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    width: [5, 7, 5],
+                    curve: 'straight',
+                    dashArray: [0, 8, 5]
+                },
+                legend: {
+                    tooltipHoverFormatter: function(val, opts) {
+                        return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
+                    }
+                },
+                markers: {
+                    size: 0,
+                    hover: {
+                        sizeOffset: 6
+                    }
+                },
+                xaxis: {
+                    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+                        '10 Jan', '11 Jan', '12 Jan'
+                    ],
+                },
+                tooltip: {
+                    y: [
+                        {
+                            title: {
+                                formatter: function (val) {
+                                return val + " (mins)"
+                                }
+                            }
+                        },
+                        {
+                            title: {
+                                formatter: function (val) {
+                                return val + " per session"
+                                }
+                            }
+                        },
+                        {
+                            title: {
+                                formatter: function (val) {
+                                return val;
+                                }
+                            }
+                        }
+                    ]
+                },
+                grid: {
+                    borderColor: '#f1f1f1',
+                }
+            };
+
+            var Profitchart = new ApexCharts(document.querySelector("#profit-chart"), options);
+            Profitchart.render();
+
+            var chart = new ApexCharts(document.querySelector("#expense-chart"), options);
+            chart.render();
+        },
+
         pShareChart: function(){
 
             const fp = $('#date-filter')[0]._flatpickr;
